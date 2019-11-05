457b4eab89002137d5c17cd0b3232af2ed9045bc
Isaac <Isaacthedeveloper@gmail.com>
2019-11-05 13:43:49 -0500

    Initial Commit

---
+++ VirtualTradeshow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTradeshow
+//
+//  Created by Isaac Ballas on 2019-11-05.
+//  Copyright © 2019 Isaacballas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
